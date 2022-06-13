06bdd1fa28da05bc24b584cb202b433323fe52f9
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-06-13 16:00:49 -0300

    Time: 1733 ms (5.09%), Space: 17.8 MB (11.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        def merge(nums1,nums2):
+            merged = []
+            i,j = 0,0
+            while i< len(nums1) and j < len(nums2):
+                if nums1[i] < nums2[j]:
+                    merged.append(nums1[i])
+                    i += 1
+                else:
+                    merged.append(nums2[j])
+                    j += 1
+            nums1_tail = nums1[i:]
+            nums2_tail = nums2[j:]
+            return merged + nums1_tail + nums2_tail
+        def merge_sort(nums):
+            if len(nums) <= 1:
+                return nums
+            mid = len(nums) // 2
+            left = nums[:mid]
+            right = nums[mid:]
+            left_sorted, right_sorted = merge_sort(left), merge_sort(right)
+            sorted_nums = merge(left_sorted, right_sorted)
+            return sorted_nums
+        return merge_sort(list(s)) == merge_sort(list(t))
